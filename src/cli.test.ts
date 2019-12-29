fc52d5842698fbefd216f10499da3b4fedc98d99
Carlos Forero <ecarlosforero@gmail.com>
2019-12-29 17:38:07 -0500

    Test Cli init command

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { command } from './cli';
 
 describe("Cli", () => {
@@ -10,6 +11,22 @@ describe("Cli", () => {
     }) as string;
 
     expect((new RegExp(/Init a leptons config file/)).test(output)).toBe(true);
+  });
+
+  test("Cli should init the configuration file", async () => {
+    const filePath = './tmp/leptons.yaml'
+
+    if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+
+    const log = jest.spyOn(global.console, 'log')
+    await new Promise((resolve) => {
+      command.parse('init -f ' + filePath, (err: any, args: any, output: any) => {
+        resolve(output);
+      })
+    });
+
+    expect(fs.existsSync(filePath)).toBe(true);
+    expect(log).toHaveBeenCalledWith(expect.stringMatching(/The leptons configuration file/));
   });
 
 });
