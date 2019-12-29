0d925937227365d3a364bbecb1b4953b2dd15090
Carlos Forero <ecarlosforero@gmail.com>
2019-12-28 19:21:05 -0500

    Improve FontSize module test

--- src/default/font-size.test.ts
+++ src/default/font-size.test.ts
@@ -16,6 +16,33 @@ describe("Builder", () => {
     const builder = new Builder();
     let output = builder.build(plainConfig);
 
-    expect(output.trim().length).toBeGreaterThan(0);
+    const expectedOutput = `
+/* Module: font-size */
+.f0_5 { font-size: 0.5rem }
+.f1 { font-size: 1rem }
+.f1_5 { font-size: 1.5rem }
+
+/* Breakpoint: m */
+@media screen and (min-width: 48rem) {
+
+  /* Module: font-size - breakpoint: m */
+  .f0_5-m { font-size: 0.5rem }
+  .f1-m { font-size: 1rem }
+  .f1_5-m { font-size: 1.5rem }
+
+}
+
+/* Breakpoint: l */
+@media screen and (min-width: 64rem) {
+
+  /* Module: font-size - breakpoint: l */
+  .f0_5-l { font-size: 0.5rem }
+  .f1-l { font-size: 1rem }
+  .f1_5-l { font-size: 1.5rem }
+
+}
+`
+
+    expect(output.trim()).toBe(expectedOutput.trim());
   });
 });
