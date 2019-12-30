643fa6717ba24c5427a9138491640092014a00f7
Carlos Forero <ecarlosforero@gmail.com>
2019-12-30 00:32:57 -0500

    Fix padding module implementation

--- src/default/padding.ts
+++ src/default/padding.ts
@@ -1,21 +1,37 @@
 import { Module } from '../module';
 import { BuildContext } from '../build-context';
+import { numberToName } from '../builder-helper';
 
 export const padding: Module = {
   name: 'padding',
-  prefix: 'f',
-  useShortName: 'inapplicable',
+  prefix: 'p',
+  useShortName: true,
   value: [0.25, 0.50, 0.75, 1, 1.5, 2, 3, 4, 6, 8, 12],
+  initExplicit: true,
 
   build: (context: BuildContext) => {
 
     const sizes = context.value as number[];
 
-    sizes.forEach(size => {
-      const sizeString = size.toString();
-      const name = sizeString.replace(new RegExp('.', 'g'), '_');
-      context.append(name, 'font-size: ' + sizeString);
-    });
+    [
+      ['all', 'a', (s: number) => `padding: ${s}`],
+      ['vertical', 'v', (s: number) => `padding-top: ${s}; padding-bottom: ${s}`],
+      ['horizontal', 'h', (s: number) => `padding-left: ${s}; padding-right: ${s}`],
+      ['top', 't', (s: number) => `padding-top: ${s}`],
+      ['bottom', 'b', (s: number) => `padding-bottom: ${s}`],
+      ['left', 'l', (s: number) => `padding-left: ${s}`],
+      ['right', 'r', (s: number) => `padding-right: ${s}`]
+    ].forEach(values => {
+      const name = values[0] as string;
+      const shortName = values[1] as string;
+      const style = values[2] as (s: number) => string;
+      sizes.forEach(size => {
+        const suffix = numberToName(size);
+
+        context.appendWithShort(
+          `${name}-${suffix}`, `${shortName}${suffix}`, style(size))
+      });
+    })
 
   }
 
