f934fff2ade9bdd359caee8a1fee689e91130e9d
Carlos Forero <ecarlosforero@gmail.com>
2019-12-30 09:36:43 -0500

    Test colors module

---
+++ src/default/colors.test.ts
@@ -0,0 +1,41 @@
+import { Builder } from "../builder";
+
+describe('Colors module', () => {
+
+  test('shoud generate css', () => {
+
+    const plainConfig = {
+      package: 'default',
+      breakpoints: {m: 48},
+      includeAll: false,
+      modules: [
+        { colors:
+          {
+            black: '#000000',
+            white: '#ffffff'
+          }
+        }
+      ]
+    }
+
+    const builder = new Builder();
+    let output = builder.build(plainConfig);
+
+    const expectedOutput = `
+/* Module: colors */
+.black { color: #000000; }
+.white { color: #ffffff; }
+
+/* Breakpoint: m */
+@media screen and (min-width: 48rem) {
+
+  /* Module: colors - breakpoint: m */
+  .black-m { color: #000000; }
+  .white-m { color: #ffffff; }
+
+}
+`
+
+    expect(output.trim()).toBe(expectedOutput.trim());
+  });
+});
