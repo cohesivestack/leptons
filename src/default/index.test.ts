6798c7f64f70610eef5f78c18664d84caa8a4a51
Carlos Forero <ecarlosforero@gmail.com>
2019-12-30 00:33:23 -0500

    Test padding module

--- src/default/index.test.ts
+++ src/default/index.test.ts
@@ -14,6 +14,7 @@ breakpoints:
 modules:
   - colors: {lightest-red: '#ffebee', light-red: '#ef9a9a', red: '#f44336', dark-red: '#d32f2f', darkest-red: '#b71c1c'}
   - font-size: [0.5, 0.75, 1, 1.5, 2, 3, 4, 6]
+  - padding: [0.25, 0.5, 0.75, 1, 1.5, 2, 3, 4, 6, 8, 12]
 `;
 
     expect(output.trim()).toBe(expectedOutput.trim());
