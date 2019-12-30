ffa46ede27603fa6a169fc053578badc8a1eb21f
Carlos Forero <ecarlosforero@gmail.com>
2019-12-30 12:34:18 -0500

    Fix style for display module

--- src/default/display.ts
+++ src/default/display.ts
@@ -9,17 +9,17 @@ export const display: Module = {
   build: (context: BuildContext) => {
 
     context
-      .appendWithShort('none', 'n', 'display: none')
-      .appendWithShort('inline', 'i', 'display: inline')
-      .appendWithShort('block', 'b', 'display: block')
-      .appendWithShort('inline-block', 'ib', 'display: inline-block')
-      .appendWithShort('inline-table', 'it', 'display: inline-table')
-      .appendWithShort('table', 't', 'display: table')
-      .appendWithShort('table-row', 'tr', 'display: table-row')
-      .appendWithShort('table-row-group', 'trg', 'display: table-row-group')
-      .appendWithShort('table-column', 'tc', 'display: table-column')
-      .appendWithShort('table-column-group', 'tcg', 'display: table-column-group')
-      .appendWithShort('table-cell', 't-cell', 'display: table-cell');
+      .appendWithShort('none', 'n', 'display: none;')
+      .appendWithShort('inline', 'i', 'display: inline;')
+      .appendWithShort('block', 'b', 'display: block;')
+      .appendWithShort('inline-block', 'ib', 'display: inline-block;')
+      .appendWithShort('inline-table', 'it', 'display: inline-table;')
+      .appendWithShort('table', 't', 'display: table;')
+      .appendWithShort('table-row', 'tr', 'display: table-row;')
+      .appendWithShort('table-row-group', 'trg', 'display: table-row-group;')
+      .appendWithShort('table-column', 'tc', 'display: table-column;')
+      .appendWithShort('table-column-group', 'tcg', 'display: table-column-group;')
+      .appendWithShort('table-cell', 't-cell', 'display: table-cell;');
 
   }
 }
