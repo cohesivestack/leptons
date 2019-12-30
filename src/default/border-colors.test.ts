250d1f27e1ac2a356f65a1d08c705242929cd9a3
Carlos Forero <ecarlosforero@gmail.com>
2019-12-30 10:07:38 -0500

    Test BorderColors module

---
+++ src/default/border-colors.test.ts
@@ -0,0 +1,43 @@
+import { Builder } from "../builder";
+
+describe('Border Colors', () => {
+
+  test('border-colors shoud generate css', () => {
+
+    const plainConfig = {
+      package: 'default',
+      breakpoints: {m: 48},
+      includeAll: false,
+      modules: [
+        { colors: { black: '#000000', white: '#ffffff' } },
+        { 'border-colors': 'default' }
+      ]
+    }
+
+    const builder = new Builder();
+    let output = builder.build(plainConfig);
+
+    const expectedOutput = `
+/* Module: colors */
+.black { color: #000000; }
+.white { color: #ffffff; }
+/* Module: border-colors */
+.bc-black { border-color: #000000; }
+.bc-white { border-color: #ffffff; }
+
+/* Breakpoint: m */
+@media screen and (min-width: 48rem) {
+
+  /* Module: colors - breakpoint: m */
+  .black-m { color: #000000; }
+  .white-m { color: #ffffff; }
+  /* Module: border-colors - breakpoint: m */
+  .bc-black-m { border-color: #000000; }
+  .bc-white-m { border-color: #ffffff; }
+
+}
+`
+
+    expect(output.trim()).toBe(expectedOutput.trim());
+  });
+});
