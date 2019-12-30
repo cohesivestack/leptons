87db77f728a6abb3e25117c57fa0e317e233ae31
Carlos Forero <ecarlosforero@gmail.com>
2019-12-30 12:07:19 -0500

    Test BorderWidths module

---
+++ src/default/border-widths.test.ts
@@ -0,0 +1,52 @@
+import { Builder } from "../builder";
+
+describe("BorderWidths module", () => {
+
+  test("shoud be created", () => {
+
+    const plainConfig = {
+      package: "default",
+      breakpoints: {m: 48},
+      includeAll: false,
+      modules: [
+        { "border-widths": [0.5, 1] }
+      ]
+    }
+
+    const builder = new Builder();
+    let output = builder.build(plainConfig);
+
+    const expectedOutput = `
+/* Module: border-widths */
+.bwa0_5 { border-width: 0.5rem; }
+.bwa1 { border-width: 1rem; }
+.bwt0_5 { border-top-width: 0.5rem; }
+.bwt1 { border-top-width: 1rem; }
+.bwb0_5 { border-bottom-width: 0.5rem; }
+.bwb1 { border-bottom-width: 1rem; }
+.bwl0_5 { border-left-width: 0.5rem; }
+.bwl1 { border-left-width: 1rem; }
+.bwr0_5 { border-right-width: 0.5rem; }
+.bwr1 { border-right-width: 1rem; }
+
+/* Breakpoint: m */
+@media screen and (min-width: 48rem) {
+
+  /* Module: border-widths - breakpoint: m */
+  .bwa0_5-m { border-width: 0.5rem; }
+  .bwa1-m { border-width: 1rem; }
+  .bwt0_5-m { border-top-width: 0.5rem; }
+  .bwt1-m { border-top-width: 1rem; }
+  .bwb0_5-m { border-bottom-width: 0.5rem; }
+  .bwb1-m { border-bottom-width: 1rem; }
+  .bwl0_5-m { border-left-width: 0.5rem; }
+  .bwl1-m { border-left-width: 1rem; }
+  .bwr0_5-m { border-right-width: 0.5rem; }
+  .bwr1-m { border-right-width: 1rem; }
+
+}
+`
+
+    expect(output.trim()).toBe(expectedOutput.trim());
+  });
+});
--- src/default/index.test.ts
+++ src/default/index.test.ts
@@ -13,6 +13,7 @@ breakpoints:
   xl: 80
 modules:
   - colors: {lightest-red: '#ffebee', light-red: '#ef9a9a', red: '#f44336', dark-red: '#d32f2f', darkest-red: '#b71c1c'}
+  - border-widths: [0.0625, 0.125, 0.25, 0.5, 1]
   - font-size: [0.5, 0.75, 1, 1.5, 2, 3, 4, 6]
   - padding: [0.25, 0.5, 0.75, 1, 1.5, 2, 3, 4, 6, 8, 12]
 `;
