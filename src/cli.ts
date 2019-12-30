82cfd2e0373c7ccb41fa998cd0848e952edf1a1c
Carlos Forero <ecarlosforero@gmail.com>
2019-12-29 22:41:12 -0500

    Add build command in cli

--- src/cli.ts
+++ src/cli.ts
@@ -2,17 +2,37 @@ import program from 'commander';
 import { Builder } from './builder';
 
 program.version('0.0.1')
+
+program
   .command('init [file_path]')
   .description('init a leptons config file. Defaults to ./leptons.yaml')
-  .action(filePath => {
-    if (!filePath) filePath = './leptons.yaml'
-    const builder = new Builder();
-    builder.init(filePath as string);
-    console.log("The leptons configuration file '" + filePath + "' was created!\n" +
+  .action(configPath => {
+
+    configPath = configPath || './leptons.yaml';
+
+    (new Builder()).init(configPath);
+
+    console.log("The leptons configuration file '" + configPath + "' was created!\n" +
       "Edit it or run:\n" +
       "  'leptons build'\n" +
       "  'leptons build > mystyles.css'\n" +
       "  'leptons build -o mystyles.css'\n");
+
+  });
+
+program
+  .command('build [file_path]')
+  .description('build a leptons css file. Defaults to ./leptons.css')
+  .option("-c, --config [file_path]", "set the config file path. Defaults to ./leptons.yaml")
+  .action((cssPath, options) => {
+
+    cssPath = cssPath || './leptons.css';
+    const configPath = options.config || './leptons.yaml';
+
+    (new Builder()).buildFromFileToFile(configPath, cssPath);
+
+    console.log("The leptons css file '" + cssPath + "' was created!");
+
   });
 
 program.parse(process.argv)
