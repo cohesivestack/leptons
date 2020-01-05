ac4de03946781416e91ca023ac0842eca2244699
Carlos Forero <ecarlosforero@gmail.com>
2020-01-04 21:08:05 -0500

    Add alignment classes to flexbox module

--- src/default/flex-box.test.ts
+++ src/default/flex-box.test.ts
@@ -20,6 +20,30 @@ describe('FlexBox module', () => {
 .fbc { flex-direction: column; }
 .fbr { flex-direction: row; }
 .fbw { flex-wrap: wrap; }
+.fbj-start { justify-content: flex-start; }
+.fbj-end { justify-content: flex-end; }
+.fbj-center { justify-content: center; }
+.fbj-stretch { justify-content: stretch; }
+.fbj-space-between { justify-content: space-between; }
+.fbj-space-around { justify-content: space-around; }
+.fbj-space-evenly { justify-content: space-evenly; }
+.fbi-start { align-items: flex-start; }
+.fbi-end { align-items: flex-end; }
+.fbi-center { align-items: center; }
+.fbi-stretch { align-items: stretch; }
+.fbi-baseline { align-items: baseline; }
+.fbs-start { align-self: flex-start; }
+.fbs-end { align-self: flex-end; }
+.fbs-center { align-self: center; }
+.fbs-stretch { align-self: stretch; }
+.fbs-baseline { align-self: baseline; }
+.fbc-start { align-content: flex-start; }
+.fbc-end { align-content: flex-end; }
+.fbc-center { align-content: center; }
+.fbc-stretch { align-content: stretch; }
+.fbc-space-between { align-content: space-between; }
+.fbc-space-around { align-content: space-around; }
+.fbc-space-evenly { align-content: space-evenly; }
 
 /* Breakpoint: m */
 @media screen and (min-width: 48rem) {
@@ -30,6 +54,30 @@ describe('FlexBox module', () => {
   .fbc-m { flex-direction: column; }
   .fbr-m { flex-direction: row; }
   .fbw-m { flex-wrap: wrap; }
+  .fbj-start-m { justify-content: flex-start; }
+  .fbj-end-m { justify-content: flex-end; }
+  .fbj-center-m { justify-content: center; }
+  .fbj-stretch-m { justify-content: stretch; }
+  .fbj-space-between-m { justify-content: space-between; }
+  .fbj-space-around-m { justify-content: space-around; }
+  .fbj-space-evenly-m { justify-content: space-evenly; }
+  .fbi-start-m { align-items: flex-start; }
+  .fbi-end-m { align-items: flex-end; }
+  .fbi-center-m { align-items: center; }
+  .fbi-stretch-m { align-items: stretch; }
+  .fbi-baseline-m { align-items: baseline; }
+  .fbs-start-m { align-self: flex-start; }
+  .fbs-end-m { align-self: flex-end; }
+  .fbs-center-m { align-self: center; }
+  .fbs-stretch-m { align-self: stretch; }
+  .fbs-baseline-m { align-self: baseline; }
+  .fbc-start-m { align-content: flex-start; }
+  .fbc-end-m { align-content: flex-end; }
+  .fbc-center-m { align-content: center; }
+  .fbc-stretch-m { align-content: stretch; }
+  .fbc-space-between-m { align-content: space-between; }
+  .fbc-space-around-m { align-content: space-around; }
+  .fbc-space-evenly-m { align-content: space-evenly; }
 
 }
 `
--- src/default/flex-box.ts
+++ src/default/flex-box.ts
@@ -9,11 +9,39 @@ export const flexBox: Module = {
   build: (context: BuildContext) => {
 
     context
-      .appendWithShort('auto', 'a', 'flex: 1 1 auto; min-width:0; min-height:0;')
-      .appendWithShort('none', 'n', 'flex: none;')
-      .appendWithShort('column', 'c', 'flex-direction: column;')
-      .appendWithShort('row', 'r', 'flex-direction: row;')
-      .appendWithShort('wrap', 'w', 'flex-wrap: wrap;');
+      .appendWithShort('-auto', 'a', 'flex: 1 1 auto; min-width:0; min-height:0;')
+      .appendWithShort('-none', 'n', 'flex: none;')
+      .appendWithShort('-column', 'c', 'flex-direction: column;')
+      .appendWithShort('-row', 'r', 'flex-direction: row;')
+      .appendWithShort('-wrap', 'w', 'flex-wrap: wrap;')
+
+      .appendWithShort('-justify-start', 'j-start', 'justify-content: flex-start;')
+      .appendWithShort('-justify-end', 'j-end', 'justify-content: flex-end;')
+      .appendWithShort('-justify-center', 'j-center', 'justify-content: center;')
+      .appendWithShort('-justify-stretch', 'j-stretch', 'justify-content: stretch;')
+      .appendWithShort('-justify-space-between', 'j-space-between', 'justify-content: space-between;')
+      .appendWithShort('-justify-space-around', 'j-space-around', 'justify-content: space-around;')
+      .appendWithShort('-justify-space-evenly', 'j-space-evenly', 'justify-content: space-evenly;')
+
+      .appendWithShort('-items-start', 'i-start', 'align-items: flex-start;')
+      .appendWithShort('-items-end', 'i-end', 'align-items: flex-end;')
+      .appendWithShort('-items-center', 'i-center', 'align-items: center;')
+      .appendWithShort('-items-stretch', 'i-stretch', 'align-items: stretch;')
+      .appendWithShort('-items-baseline', 'i-baseline', 'align-items: baseline;')
+
+      .appendWithShort('-self-start', 's-start', 'align-self: flex-start;')
+      .appendWithShort('-self-end', 's-end', 'align-self: flex-end;')
+      .appendWithShort('-self-center', 's-center', 'align-self: center;')
+      .appendWithShort('-self-stretch', 's-stretch', 'align-self: stretch;')
+      .appendWithShort('-self-baseline', 's-baseline', 'align-self: baseline;')
+
+      .appendWithShort('-content-start', 'c-start', 'align-content: flex-start;')
+      .appendWithShort('-content-end', 'c-end', 'align-content: flex-end;')
+      .appendWithShort('-content-center', 'c-center', 'align-content: center;')
+      .appendWithShort('-content-stretch', 'c-stretch', 'align-content: stretch;')
+      .appendWithShort('-content-space-between', 'c-space-between', 'align-content: space-between;')
+      .appendWithShort('-content-space-around', 'c-space-around', 'align-content: space-around;')
+      .appendWithShort('-content-space-evenly', 'c-space-evenly', 'align-content: space-evenly;')
 
   }
 }
