5ac190032056b1b10812897d05a4726382110d36
Carlos Forero <ecarlosforero@gmail.com>
2020-03-14 14:12:38 -0500

    Implement Builder functions

--- src/builder.test.ts
+++ src/builder.test.ts
@@ -1,5 +1,28 @@
+import { buildFromPackagesAndClasses } from "./builder";
+import { initPackage } from "./default";
+
 describe("Builder", () => {
-  test("dummy", () => {
-    expect(true);
+  test("should create css output", () => {
+
+    const classes = [
+      "p-h-1",
+      "p-v-2px",
+      "p-v-4px-M",
+      "p-v-8px-L",
+    ];
+
+    const pkg = initPackage({M: 32, L: 64});
+    const css = buildFromPackagesAndClasses([pkg], classes);
+
+    expect(css.trim()).toBe(
+`.p-h-1 { padding-left: 1rem; padding-right: 1rem; }
+.p-v-2px { padding-top: 2px; padding-bottom: 2px; }
+@media screen and (min-width: 32rem) {
+  .p-v-4px-M { padding-top: 4px; padding-bottom: 4px; }
+}
+@media screen and (min-width: 64rem) {
+  .p-v-8px-L { padding-top: 8px; padding-bottom: 8px; }
+}`);
+
   });
 });
--- src/builder.ts
+++ src/builder.ts
@@ -1,28 +1,58 @@
 import * as Helper from "./builder-helper";
 import { isConfigErrors, ConfigError, Config } from "./config";
+import { initPackage } from "./default";
+import { Media } from "./media";
+import { Package } from "./package";
 
-export class Builder {
+export function build(plainConfig: any): string {
 
-  private config?: Config;
+  // Validate config
+  if (isConfigErrors(plainConfig)) {
+    const errors = plainConfig as ConfigError[];
 
-  build(plainConfig: any): string {
+    let reportErrors = '';
+    errors.forEach(e => reportErrors += '  ' + e.path + ' -> ' + e.message + '\n')
 
-    // Validate config
-    if (isConfigErrors(plainConfig)) {
-      const errors = plainConfig as ConfigError[];
-
-      let reportErrors = '';
-      errors.forEach(e => {
-        reportErrors += '  ' + e.path + ' -> ' + e.message + '\n';
-      })
-
-      throw Error("Errors parsing plain object:\n" + reportErrors);
-    }
-
-    const config = this.config as Config;
-
-    const classes = Helper.distinctClasses(Helper.extractClassesFromSource(config.source));
-
-    return ""
+    throw Error("Errors parsing plain object:\n" + reportErrors);
   }
+
+  const config = plainConfig as Config;
+  const classes = Helper.distinctClasses(Helper.extractClassesFromSource(config.source));
+  const defaultPackage = initPackage(config.breakpoints, config.prefix, config.modules);
+
+  // Using only defaultPackage for now. Support for third packages is in the Roadmap
+
+  return buildFromPackagesAndClasses([defaultPackage], classes);
+}
+
+export function buildFromPackagesAndClasses(packages: Package[], classes: string[]): string {
+
+  const packageMedias = new Map<Package, Map<string | undefined, Media>>();
+
+  packages.forEach(pkg => {
+    const medias = new Map<string | undefined, Media>();
+    medias.set(undefined, new Media(pkg));
+
+    Object
+      .keys(pkg.breakpoints)
+      .forEach(name => medias.set(name, new Media(pkg, name)));
+
+      packageMedias.set(pkg, medias);
+  });
+
+  classes.forEach(className => {
+    packages.forEach(pkg => {
+      const atom = pkg.getAtom(className.split("-"));
+      if (atom) {
+        packageMedias.get(pkg)?.get(atom.breakpoint)?.addAtom(atom);
+      }
+    });
+  });
+
+  let cssString = "";
+  packages.forEach(pkg => {
+    packageMedias.get(pkg)?.forEach(media => cssString += media.build());
+  });
+
+  return cssString;
 }
