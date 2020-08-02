24d034a891cab45b8b5f7d44d3dcab7347f5e13d
Carlos Forero <ecarlosforero@gmail.com>
2020-08-02 11:53:00 -0500

    Test Module creation

---
+++ src/module.test.ts
@@ -0,0 +1,32 @@
+import { Module } from "./module";
+import { Style } from "./style";
+import { BuilderContext } from "./builder-context";
+
+describe("Atom", () => {
+  test("Create module with all types of styles", () => {
+
+    const styles: { [key: string]: Style } = {
+      "s-a":         "background-size: auto;",
+      "p-{length}":  "background-position: {length};",
+      "w-{weight}": ["font-weight: {weight}", (c: BuilderContext, v: string) => c.convertNumberPerHundrerToCss(v)],
+      "u-{custom}":   (c: BuilderContext, v: string) => `unknown: ${v}`,
+    }
+
+    const module = new Module(
+      "Test",
+      "t",
+      styles);
+
+    expect(module.getLiteral("s-a")).toBe(styles["s-a"]);
+
+    const item = module.getItem("p");
+    expect(item?.itemName).toBe("length");
+    expect(item?.style).toBe(styles["p-{length}"]);
+
+    const itemFunction = module.getItemFunction("w");
+    expect(itemFunction?.itemName).toBe("weight");
+    expect(itemFunction?.style).toBe(styles["w-{weight}"]);
+
+    expect(module.getFunction("w")).toBe(styles["w"]);
+  });
+});
