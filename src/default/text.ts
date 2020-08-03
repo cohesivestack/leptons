cf1f992818e22bb9c9c7215b0a8ac768719e191b
Carlos Forero <ecarlosforero@gmail.com>
2020-08-03 16:41:49 -0500

    Fix Text module

--- src/default/text.ts
+++ src/default/text.ts
@@ -14,44 +14,44 @@ const styles: any = {
   "a-justify": "text-align: jusitify;",
 
   // Decoration Line
-  "dl-u":            "text-decoration-line: underline",
-  "dl-underline":    "text-decoration-line: underline",
-  "dl-o":            "text-decoration-line: overline",
-  "dl-overline":     "text-decoration-line: overline",
-  "dl-lt":           "text-decoration-line: line-through",
-  "dl-line-through": "text-decoration-line: line-through",
-  "dl-n":            "text-decoration-line: none",
-  "dl-none":         "text-decoration-line: none",
+  "dl-u":            "text-decoration-line: underline;",
+  "dl-underline":    "text-decoration-line: underline;",
+  "dl-o":            "text-decoration-line: overline;",
+  "dl-overline":     "text-decoration-line: overline;",
+  "dl-lt":           "text-decoration-line: line-through;",
+  "dl-line-through": "text-decoration-line: line-through;",
+  "dl-n":            "text-decoration-line: none;",
+  "dl-none":         "text-decoration-line: none;",
 
   // Decoration Style
-  "ds-s":      "text-decoration-style: solid",
-  "ds-solid":  "text-decoration-style: solid",
-  "ds-d":      "text-decoration-style: dotted",
-  "ds-dotted": "text-decoration-style: dotted",
-  "ds-dashed": "text-decoration-style: dashed",
-  "ds-w":      "text-decoration-style: wavy",
+  "ds-s":      "text-decoration-style: solid;",
+  "ds-solid":  "text-decoration-style: solid;",
+  "ds-d":      "text-decoration-style: dotted;",
+  "ds-dotted": "text-decoration-style: dotted;",
+  "ds-dashed": "text-decoration-style: dashed;",
+  "ds-w":      "text-decoration-style: wavy;",
 
   // Justify
-  "j-a":              "text-justify: auto",
-  "j-auto":           "text-justify: auto",
-  "j-iw":             "text-justify: inter-word",
-  "j-interWord":      "text-justify: inter-word",
-  "j-ic":             "text-justify: inter-character",
-  "j-interCharacter": "text-justify: inter-character",
-  "j-n":              "text-justify: none",
-  "j-none":           "text-justify: none",
+  "j-a":              "text-justify: auto;",
+  "j-auto":           "text-justify: auto;",
+  "j-iw":             "text-justify: inter-word;",
+  "j-interWord":      "text-justify: inter-word;",
+  "j-ic":             "text-justify: inter-character;",
+  "j-interCharacter": "text-justify: inter-character;",
+  "j-n":              "text-justify: none;",
+  "j-none":           "text-justify: none;",
 
   // Overflow
-  "o-c":        "text-overflow: clip",
-  "o-clip":     "text-overflow: clip",
-  "o-e":        "text-overflow: ellipsis",
-  "o-ellipsis": "text-overflow: ellipsis",
+  "o-c":        "text-overflow: clip;",
+  "o-clip":     "text-overflow: clip;",
+  "o-e":        "text-overflow: ellipsis;",
+  "o-ellipsis": "text-overflow: ellipsis;",
 
   // Transform
-  "t-u": "text-transform: uppercase",
-  "t-l": "text-transform: lowercase",
-  "t-c": "text-transform: capitalize",
-  "t-n": "text-transform: none"
+  "t-u": "text-transform: uppercase;",
+  "t-l": "text-transform: lowercase;",
+  "t-c": "text-transform: capitalize;",
+  "t-n": "text-transform: none;"
 }
 
 export class Text extends Module {
