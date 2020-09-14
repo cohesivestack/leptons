4e95bcd28c7706847ef51b7c498580626154e013
Carlos Forero <ecarlosforero@gmail.com>
2020-09-14 12:51:23 -0500

    Add build method to Media

--- src/media.ts
+++ src/media.ts
@@ -1,4 +1,27 @@
-export type Media = {
-  rule: string,
-  classes: { [className: string]: string }
+export class Media {
+  constructor(
+    public rule: string,
+    public classes: { [className: string]: string }) {}
+
+  public build(): string {
+    let cssString = "";
+    let tabSpace = "";
+
+    if (this.rule !== "") {
+      cssString += `@media ${this.rule} {\n`;
+      tabSpace = "  ";
+    }
+
+
+    Object.keys(this.classes).sort((a,b) => a > b ? 1 : -1 ).forEach(className => {
+      let cssStyle = this.classes[className];
+      cssString += `${tabSpace}.${className} { ${cssStyle} }\n`
+    });
+
+    if (this.rule !== "") {
+      cssString += "}\n";
+    }
+
+    return cssString;
+  }
 }
