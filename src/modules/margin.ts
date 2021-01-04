36446bdd2e4e92d17afc5db46989b472a69c5fc7
Carlos Forero <ecarlosforero@gmail.com>
2021-01-03 21:41:33 -0500

    Add auto value to Margin module

--- src/modules/margin.ts
+++ src/modules/margin.ts
@@ -9,9 +9,16 @@ const styles: { [key: string]: Style } = {
   "t-{length}": "margin-top: {length};",
   "r-{length}": "margin-right: {length};",
   "b-{length}": "margin-bottom: {length};",
-  "l-{length}": "margin-bottom: {length};",
+  "l-{length}": "margin-left: {length};",
   "v-{length}": "margin-top: {length}; margin-bottom: {length};",
-  "h-{length}": "margin-left: {length}; margin-right: {length};"
+  "h-{length}": "margin-left: {length}; margin-right: {length};",
+  "a": "margin: auto;",
+  "t-a": "margin-top: auto;",
+  "r-a": "margin-right: auto;",
+  "b-a": "margin-bottom: auto;",
+  "l-a": "margin-left: auto;",
+  "v-a": "margin-top: auto; margin-bottom: auto;",
+  "h-a": "margin-left: auto; margin-right: auto;"
 }
 
 export const margin = new Module(name, symbol, styles);
