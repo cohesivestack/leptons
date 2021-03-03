36e3793802c3d45713ec8958c796ea60f482fc5a
Miguel Forero V <miguelforero@gmail.com>
2021-03-02 22:35:20 -0500

    WIP: Add transition module

---
+++ src/modules/transition.ts
@@ -0,0 +1,21 @@
+import { Style } from "../style";
+import { Module } from "../module";
+
+const name: string = "Transition";
+const symbol: string = "trans";
+
+const styles: { [key: string]: Style } = {
+
+  //Transition Property
+  "n":          "transition-property: none;",
+  "none":       "transition-property: none;",
+  "a":          "transition-property: all;",
+  "all":        "transition-property: all;",
+
+  //TO DO
+
+  "{keyword}":  "transition-property: {keyword};",
+
+}
+
+export const outline = new Module(name, symbol, styles);
