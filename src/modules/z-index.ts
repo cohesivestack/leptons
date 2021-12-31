2ea649ebcc6b033bc683224544b2d5977ef9f71f
Carlos Forero <ecarlosforero@gmail.com>
2021-12-31 11:04:51 -0500

    Complete z-index module

--- src/modules/z-index.ts
+++ src/modules/z-index.ts
@@ -1,18 +1,14 @@
 import { Style } from "../style";
-import { BuilderContext } from "../builder-context";
 import { Module } from "../module";
 
 const name: string = "Z-Index"
 const symbol: string = "z";
 
 const styles: { [key: string]: Style } = {
-  "{index}": ["z-index: {index}", (c: BuilderContext, v: string) => {
-    if (!/^\d+$/.test(v)) {
-      throw new Error(`The z-index ${v} is not valid`);
-    }
-    return v;
-  }],
-  "{keyword}": "{keyword}"
+  "a":         "cursor: auto;",
+  "auto":      "cursor: auto;",
+  "{number}":  "z-index: {number};",
+  "{keyword}": "z-index: {keyword};"
 }
 
 export const zIndex = new Module(name, symbol, styles);
