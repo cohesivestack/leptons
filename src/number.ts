00493bcb1b9fa12f886ed0ce3d3a4576285bd227
Carlos Forero <ecarlosforero@gmail.com>
2022-01-25 15:31:20 -0500

    Add convertNumberToCss function

--- src/number.ts
+++ src/number.ts
@@ -3,3 +3,10 @@ const regexpNumberValid = new RegExp("^[0-9]+(\.[0-9]+)?$");
 export const isNumberValid = (number: string): boolean => {
   return regexpNumberValid.test(number);
 }
+
+export const convertNumberToCss = (number: string): string => {
+  if (!isNumberValid(number)) {
+    throw new Error(`The value ${number} is not valid`);
+  }
+  return number;
+}
