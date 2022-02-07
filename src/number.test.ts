99c5a7af4ac73a90a5bf89ac0401b7c82810242c
Carlos Forero <ecarlosforero@gmail.com>
2022-02-06 19:17:56 -0500

    Add support to negative numbers

--- src/number.test.ts
+++ src/number.test.ts
@@ -7,7 +7,7 @@ describe("Number", () => {
   test("atomToCssStyle should create css styles with numbers", () => {
 
     const styles: { [key: string]: string } = {
-      "s-{number}":  "size: {number};"
+      "s-{n$number}":  "size: {n};"
     }
 
     const module = new Module(
@@ -18,14 +18,20 @@ describe("Number", () => {
     const builder = new Builder();
     builder.addModule(module);
 
-    const cssItem = new Atom("t-s-2", builder).toCss(module)[""].cssStyle;
+    let cssItem = new Atom("t-s-2", builder).toCss(module)[""].cssStyle;
     expect(cssItem).toBe("size: 2;");
+
+    cssItem = new Atom("t-s-n2", builder).toCss(module)[""].cssStyle;
+    expect(cssItem).toBe("size: -2;");
   });
 
   test("isNumberValid should validate number", () => {
     expect(isNumberValid("2")).toBe(true);
     expect(isNumberValid("2.1")).toBe(true);
+    expect(isNumberValid("n2")).toBe(true);
+    expect(isNumberValid("n2.1")).toBe(true);
     expect(isNumberValid("2px")).toBe(false);
+    expect(isNumberValid("n2px")).toBe(false);
     expect(isNumberValid("x")).toBe(false);
   });
 });
--- src/number.ts
+++ src/number.ts
@@ -1,4 +1,4 @@
-const regexpNumberValid = new RegExp("^[0-9]+(\.[0-9]+)?$");
+const regexpNumberValid = new RegExp("^n?[0-9]+(\.[0-9]+)?$");
 
 export const isNumberValid = (number: string): boolean => {
   return regexpNumberValid.test(number);
@@ -8,5 +8,10 @@ export const convertNumberToCss = (number: string): string => {
   if (!isNumberValid(number)) {
     throw new Error(`The value ${number} is not valid`);
   }
+
+  if (/^n/.test(number)) {
+    number = number.replace("n", "-");
+  }
+
   return number;
 }
